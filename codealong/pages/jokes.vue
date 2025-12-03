90ccab6414c45975c1ed7be6c9db7833830f7b7d
Daniel Andreas Wang <danielaw@getacademy.no>
2025-12-03 19:46:59 +0100

    send jokes

---
+++ codealong/pages/jokes.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { data, pending, error, refresh } = useFetch('/api/jokes')
+</script>
+
+<template>
+  <div>
+    <h1>Vitser</h1>
+
+    <div v-if="pending">Laster vitser...</div>
+    <div v-else-if="error">Noe gikk galt: {{ error.message }}</div>
+    <div v-else>
+      <ul>
+        <li v-for="(joke, index) in data.jokes" :key="index">
+          {{ joke }}
+        </li>
+      </ul>
+
+      <button @click="refresh()">Last på nytt</button>
+    </div>
+  </div>
+</template>
---
+++ codealong/server/api/jokes.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(() => {
+  return {
+    jokes: [
+      "Hvorfor kan ikke syklister stå opp? Fordi de er to-hjulte!",
+      "Hva sier en datamus når den er lei seg? Jeg føler meg så klikket...",
+      "Hvorfor gikk JavaScript til psykologen? Fordi den hadde for mange callbacks!"
+    ]
+  };
+});
